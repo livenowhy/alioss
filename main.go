00cf340e86b6eb05f8b8fda071c5ab7cd1d02255
zhangpei.liu <liuzhangpei@126.com>
2016-11-17 16:54:42 +0800

    ....

--- main.go
+++ main.go
@@ -10,14 +10,17 @@ import (
 
 
 func main() {
-	c, err := server.LoadConfig("/Users/lzp/Desktop/WorkGo/src/github.com/liuzhangpei/alioss/conf/key.yml")
-
-	if err != nil {
-		glog.Exitf("Failed to load config: %s", err)
-	}
+	//c, err := server.LoadConfig("/Users/lzp/Desktop/WorkGo/src/github.com/liuzhangpei/alioss/conf/key.yml")
+	//
+	//if err != nil {
+	//	glog.Exitf("Failed to load config: %s", err)
+	//}
+		// 106.38.76.170
+	//fmt.Println(c.AliyunKey.AccessKeySecret)
 
 	http.HandleFunc("/", server.PolicyCallback)
 	http.HandleFunc("/callback", server.Callback)
-	http.ListenAndServe(":1234", nil)
-	fmt.Println(c.AliyunKey.AccessKeySecret)
+	http.ListenAndServe(":8765", nil)
+
+
 }
