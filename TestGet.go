2ecccc75aa3ce44d7346a90b652986fd5aa4087b
zhangpei.liu <liuzhangpei@126.com>
2016-11-23 18:23:58 +0800

    ...

--- TestGet.go
+++ TestGet.go
@@ -12,16 +12,23 @@ func HttpGet()  {
 	//u, _ := url.Parse("https://gosspublic.alicdn.com/callback_pub_key_v1.pem")
 	res, err := http.Get("https://gosspublic.alicdn.com/callback_pub_key_v1.pem")
 
+	defer res.Body.Close()
+	fmt.Println("HttpGet 01")
+
 	if err != nil {
+		fmt.Println("HttpGet 02")
 		log.Fatal(err)
 		return
 	}
 
-	fmt.Println("HttpGet sdsdsdsdsdsd")
+	fmt.Println("HttpGet 03")
 
 	result, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
+
+	fmt.Println("HttpGet 04")
+
 	if err != nil {
+		fmt.Println("HttpGet 05")
 		log.Fatal(err)
 		return
 	}
